39411d73e0a62d3b2fb288e216d6e10a537d32db
marcelfloe <marcelfloe@gmail.com>
2025-07-10 13:10:18 +0200

    Creating a JML-Annotation.

---
+++ src/main/java/jatyc/lib/JML.java
@@ -0,0 +1,17 @@
+package jatyc.lib;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({
+  ElementType.METHOD, ElementType.TYPE, ElementType.CONSTRUCTOR,
+  ElementType.ANNOTATION_TYPE, ElementType.TYPE_USE, ElementType.FIELD,
+  ElementType.LOCAL_VARIABLE, ElementType.MODULE, ElementType.PACKAGE,
+  ElementType.PARAMETER, ElementType.RECORD_COMPONENT, ElementType.TYPE_PARAMETER
+})
+public @interface JML {
+  String value();
+}
