f6d29b074de631777df0e8b5b8ecaef1e58be773
marcelfloe <marcelfloe@gmail.com>
2025-09-14 13:11:50 +0200

    Creating multiple maps which are able to query all mappings of one value at any position of the mapping. (entries with 2,3 and 4 values)

---
+++ src/main/java/jatyc/util/multimap/BiMap.java
@@ -0,0 +1,47 @@
+package jatyc.util.multimap;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class BiMap<A,B> {
+  private Map<A, Set<Entry<A,B>>> aMap = new HashMap<>();
+  private Map<B, Set<Entry<A,B>>> bMap = new HashMap<>();
+  private Set<A> as = new HashSet<>();
+  private Set<B> bs = new HashSet<>();
+
+  public BiMap() {}
+
+  public void putA(A value) {
+    as.add(value);
+    aMap.put(value, new HashSet<>());
+  }
+
+  public void putB(B value) {
+    bs.add(value);
+    bMap.put(value, new HashSet<>());
+  }
+
+  public void map(A a, B b) {
+    if (!as.contains(a)) {
+      putA(a);
+    }
+    if (!bs.contains(b)) {
+      putB(b);
+    }
+    Entry<A,B> entry = new Entry<>(a,b);
+    aMap.get(a).add(entry);
+    bMap.get(b).add(entry);
+  }
+
+  public Set<Entry<A,B>> getAMapping(A a) {
+    return aMap.get(a);
+  }
+
+  public Set<Entry<A,B>> getBMapping(B b) {
+    return bMap.get(b);
+  }
+
+  private record Entry<Y,Z> (Y y, Z z) {}
+}
---
+++ src/main/java/jatyc/util/multimap/TriMap.java
@@ -0,0 +1,62 @@
+package jatyc.util.multimap;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class TriMap <A,B,C> {
+  private Map<A, Set<Entry<A,B,C>>> aMap = new HashMap<>();
+  private Map<B, Set<Entry<A,B,C>>> bMap = new HashMap<>();
+  private Map<C, Set<Entry<A,B,C>>> cMap = new HashMap<>();
+  private Set<A> as = new HashSet<>();
+  private Set<B> bs = new HashSet<>();
+  private Set<C> cs = new HashSet<>();
+
+  public TriMap() {}
+
+  public void putA(A value) {
+    as.add(value);
+    aMap.put(value, new HashSet<>());
+  }
+
+  public void putB(B value) {
+    bs.add(value);
+    bMap.put(value, new HashSet<>());
+  }
+
+  public void putC(C value) {
+    cs.add(value);
+    cMap.put(value, new HashSet<>());
+  }
+
+  public void map(A a, B b, C c) {
+    if (!as.contains(a)) {
+      putA(a);
+    }
+    if (!bs.contains(b)) {
+      putB(b);
+    }
+    if (!cs.contains(c)) {
+      putC(c);
+    }
+    Entry<A,B,C> entry = new Entry<>(a,b,c);
+    aMap.get(a).add(entry);
+    bMap.get(b).add(entry);
+    cMap.get(c).add(entry);
+  }
+
+  public Set<Entry<A,B,C>> getAMapping(A a) {
+    return aMap.get(a);
+  }
+
+  public Set<Entry<A,B,C>> getBMapping(B b) {
+    return bMap.get(b);
+  }
+
+  public Set<Entry<A,B,C>> getCMapping(C c) {
+    return cMap.get(c);
+  }
+
+  private record Entry<X,Y,Z> (X x, Y y, Z z) {}
+}
